ad71c1b3af350b3a38048ebd7a6d1e1b1b2f2977
Kristoff1131 <45649149+Kristoff1131@users.noreply.github.com>
2020-06-04 14:19:41 -0400

    Add files via upload

---
+++ Concurrent Server (Project 2)/ClientGenerator.java
@@ -0,0 +1,178 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.concurrent.atomic.LongAdder;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ClientGenerator {
+
+
+    public static void main(String[] args){
+
+        int numOfClients;
+        int requestType;
+        String networkAddress;
+        int port;
+
+        do{
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter Network Address or Q to quit: ");
+            networkAddress = input.next();
+
+            if(networkAddress.equalsIgnoreCase("Q")){
+
+                return;
+            }
+            System.out.println("Enter Port Number: ");
+            port = input.nextInt();
+            System.out.println("Enter number of clients requests (1, 5, 10, 15, 20, 25, 100): ");
+            numOfClients = input.nextInt();
+
+            System.out.println("Request Type: " +
+                    "\n Date and Time (1) " +
+                    "\n Uptime (2) " +
+                    "\n Memory Use (3) " +
+                    "\n Netstat (4) " +
+                    "\n Current Users (5) " +
+                    "\n Running Processes (6) " +
+                    "\n Enter Request: ");
+            requestType = input.nextInt();
+
+            //create #of clients and start requests
+            for (int i = 0; i < numOfClients; i++) {
+                Client client = new Client(requestType, port, networkAddress);
+                client.start();
+
+            }
+
+            try {
+                while (Client.getActiveThreads() > 0 ) {
+                    Thread.sleep(20);
+                }
+            }catch (Exception e)
+            {
+                System.out.println("Thread counter failed, thread time may be wrong");
+            }
+
+            double sec = ((double) Client.seconds.sumThenReset())/1000000000;
+            System.out.println("Total Turn Around Time: " + sec);
+            System.out.println("Average Turn Around Time: " + sec/numOfClients);
+
+            //delay printing of user input screen.
+            try {
+                Thread.sleep(200);
+
+            }catch (Exception e)
+            {
+                System.out.println("Thread counter failed, thread time may be wrong");
+            }
+
+        }while(true);
+
+    }
+}
+class Client extends Thread{
+
+    private int request;
+    private int port;
+    private String address;
+
+    public static LongAdder seconds = new LongAdder();
+    private static int activeThreads = 0;
+    private static Lock lock = new ReentrantLock();
+
+
+    public Client(int request, int port, String address){
+        this.request = request;
+        this.port = port;
+        this.address = address;
+
+
+    }
+
+    public void run(){
+
+        long startTime = System.nanoTime();
+        try {
+            increaseCounter();
+
+            //create connection to Address&Port
+            Socket socket = new Socket(address, port);
+
+
+            //create output
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            //send output
+            writer.println(request);
+
+
+            //get request
+            InputStream input = socket.getInputStream();
+
+            //load request into buffer reader
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            //print output
+            String response;
+            while ((response = reader.readLine()) != null){
+                System.out.println(response);
+            }
+
+            writer.flush();
+            long stopTime = System.nanoTime();
+            long clientTime = stopTime - startTime;
+            double sec = ((double) clientTime)/1000000000;
+            seconds.add(clientTime);
+            System.out.println("Elapsed Time for Client: " + sec + " seconds");
+            decreaseCounter();
+
+        } catch (IOException | InterruptedException e) {
+            try {
+                decreaseCounter();
+            } catch (InterruptedException ex) {
+                System.out.println("Counter Error: " + ex.toString());
+            }
+            System.out.println("Error: "+ e.toString());
+        }
+    }
+    public int getRequest(){
+        return this.request;
+    }
+    public void setRequest(int request){
+        this.request = request;
+    }
+
+
+    private void increaseCounter() throws InterruptedException {
+        while (!lock.tryLock()){
+
+            Thread.sleep(10);
+
+        }
+        activeThreads++;
+        lock.unlock();
+    }
+    private void decreaseCounter() throws InterruptedException {
+        while (!lock.tryLock()){
+
+            Thread.sleep(10);
+
+        }
+        activeThreads--;
+        lock.unlock();
+    }
+    public static int getActiveThreads() throws InterruptedException {
+
+        while (!lock.tryLock()){
+
+            Thread.sleep(10);
+
+        }
+        int i = activeThreads;
+        lock.unlock();
+        return i;
+    }
+}
---
+++ Concurrent Server (Project 2)/ServerSide.java
@@ -0,0 +1,141 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.lang.*;
+
+public class ServerSide
+{
+    public static void main(String[] args) throws UnknownHostException {
+        //Scanner to read user input
+        Scanner s = new Scanner(System.in);
+
+        //Prompting user for IP Address
+        System.out.print("IP ADDRESS: ");
+        String ip = s.nextLine();
+
+        InetAddress ipAddr = InetAddress.getByName(ip);
+
+        //Prompting user for Port
+        System.out.print("PORT: ");
+        int port = s.nextInt();
+
+        //Multi Thread Code
+        try(ServerSocket serverSocket = new ServerSocket(port, 25, ipAddr)) {
+            System.out.println("Server is listening on port " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected...");
+
+                new ServerThread(socket).start();
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("Server exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+}
+
+class ServerThread extends Thread
+{
+    private Socket socket;
+
+    public ServerThread(Socket socket)
+    {
+        this.socket = socket;
+    }
+
+    public void run()
+    {
+        try
+        {
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output);
+
+            boolean running = true;
+
+            Process process = null;
+            String processValue = null;
+            BufferedReader br;
+
+            while(running)
+            {
+                String userIn = reader.readLine();
+
+                switch (userIn)
+                {
+                    //Date and Time - the date and time on the server
+                    case "1":
+                        process = Runtime.getRuntime().exec("date");
+                        br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        while((processValue = br.readLine()) != null)
+                        {
+                            writer.println(processValue);
+                        }
+                        break;
+                    //Uptime - how long the server has been running since last boot-up
+                    case "2":
+                        process = Runtime.getRuntime().exec("uptime");
+                        br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        while((processValue = br.readLine()) != null)
+                        {
+                            writer.println(processValue);
+                        }
+                        break;
+                        //Memory Use - the current memory usage on the server
+                    case "3":
+                        process = Runtime.getRuntime().exec("free -h -t");
+                        br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        while((processValue = br.readLine()) != null)
+                        {
+                            writer.println(processValue);
+                        }
+                        break;
+                        //Netstat - lists network connections on the server
+                    case "4":
+                        process = Runtime.getRuntime().exec("netstat -rn");
+                        br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        while((processValue = br.readLine()) != null)
+                        {
+                            writer.println(processValue);
+                        }
+                        break;
+                        //Current Users - list of users currently connected to the server
+                    case "5":
+                        process = Runtime.getRuntime().exec("w");
+                        br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        while((processValue = br.readLine()) != null)
+                        {
+                            writer.println(processValue);
+                        }
+                        break;
+                        //Running Processes - list of programs currently running on the server
+                    case "6":
+                        process = Runtime.getRuntime().exec("ps");
+                        br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                        while((processValue = br.readLine()) != null)
+                        {
+                            writer.println(processValue);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                writer.close();
+                socket.close();
+                break;
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("Server exception: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
